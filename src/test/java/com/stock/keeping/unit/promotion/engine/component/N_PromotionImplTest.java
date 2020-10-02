c8362ce720f1cc6fa8527980ef7b5976cc0f346b
Prateek Arora <aroraprateek091@outlook.com>
2020-10-02 18:46:48 +0530

    Tested the implementation for A. Encountering Null Pointer Exception

--- src/test/java/com/stock/keeping/unit/promotion/engine/component/N_PromotionImplTest.java
+++ src/test/java/com/stock/keeping/unit/promotion/engine/component/N_PromotionImplTest.java
@@ -1,5 +1,26 @@
 package com.stock.keeping.unit.promotion.engine.component;
 
+
+import com.stock.keeping.unit.promotion.engine.bean.StockKeepingUnit;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 class N_PromotionImplTest {
+
+    @Mock
+    StockKeepingUnit stockKeepingUnit;
+
+    @Test
+    public void shouldCalculateForA(){
+        when(stockKeepingUnit.getId()).thenReturn('A');
+        verify(stockKeepingUnit.getId(), atLeast(1));
+        Assert.assertEquals(new N_PromotionImpl().price(stockKeepingUnit,3),130);
+    }
 
 }
