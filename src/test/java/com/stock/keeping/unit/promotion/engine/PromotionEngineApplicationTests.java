2f5daa90e5f97ee11657df7e069f986acf6211bd
Prateek Arora <aroraprateek091@outlook.com>
2020-10-02 15:50:04 +0530

    Initial Commit: Got project imported from Sping Initializr. This is not a web project though

---
+++ src/test/java/com/stock/keeping/unit/promotion/engine/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.keeping.unit.promotion.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
