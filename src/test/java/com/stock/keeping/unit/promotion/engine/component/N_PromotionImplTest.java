0f780570ed446f22d7f11b06ee1c2ba23c8b066a
Prateek Arora <aroraprateek091@outlook.com>
2020-10-02 20:07:10 +0530

    Rollback previous commit

--- src/test/java/com/stock/keeping/unit/promotion/engine/component/N_PromotionImplTest.java
+++ src/test/java/com/stock/keeping/unit/promotion/engine/component/N_PromotionImplTest.java
@@ -5,25 +5,18 @@ import com.stock.keeping.unit.promotion.engine.bean.StockKeepingUnit;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
-import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 class N_PromotionImplTest {
 
-    @Mock
-    StockKeepingUnit stockKeepingUnit;
+
     StockKeepingUnit stockKeepingUnitA = new StockKeepingUnit('A',50);
     StockKeepingUnit stockKeepingUnitB = new StockKeepingUnit('A',50);
 
     @Test
     public void shouldCalculateForA(){
-        stockKeepingUnit = mock(StockKeepingUnit.class);
-        when(stockKeepingUnit.getId()).thenReturn('A');
-        verify(stockKeepingUnit.getId(),atLeastOnce());
-        Assert.assertEquals(new N_PromotionImpl().price(stockKeepingUnit,3),130);
+        Assert.assertEquals(new N_PromotionImpl().price(stockKeepingUnitA,3),130);
     }
 
     @Test
